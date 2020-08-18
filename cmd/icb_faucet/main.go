4ef96a655c09340587c2ab8347612238f2a68c2c
Yuki Tanaka <yukitanaka@Yuki-no-MacBook-Pro.local>
2020-08-18 17:51:09 +0900

    slightly change

--- cmd/icb_faucet/main.go
+++ cmd/icb_faucet/main.go
@@ -28,5 +28,5 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
             http.NotFound(w, r)
             return
     }
-    fmt.Fprint(w, "Hello, World!")
+    fmt.Fprint(w, "Hello, World!gae")
 }
