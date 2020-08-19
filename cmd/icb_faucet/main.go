b81b321333353c09b393da8282c75069b499ea7c
Yuki Tanaka <j1307099@yahoo.co.jp>
2020-08-19 18:18:10 +0900

    token balance

--- cmd/icb_faucet/main.go
+++ cmd/icb_faucet/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
     "fmt"
+    "context"
     "log"
     "net/http"
     "os"
+    "github.com/ethereum/go-ethereum/accounts/abi/bind"
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/ethclient"
+    token "github.com/youkchan/icb_faucet/pkg/token"
 )
 
 func main() {
@@ -28,5 +33,31 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
             http.NotFound(w, r)
             return
     }
-    fmt.Fprint(w, "Hello, World!gae")
+
+    client, err := ethclient.Dial("https://rinkeby.infura.io/v3/deac92b380cd4b219b0c57a59cf363b1")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+
+    rinkebyICBAddress := common.HexToAddress("0x5446E3481e3fe4b3082067145A47d7a0F09d5E1A")
+    rinkebyICBInstance, err := token.NewToken(rinkebyICBAddress, client)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    account := common.HexToAddress("0x751e0e0de1881f614F40C14c175bdd12d0DCaa24")
+    balance, err := client.BalanceAt(context.Background(), account, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(balance)
+
+    bal, err := rinkebyICBInstance.BalanceOf(&bind.CallOpts{}, account)
+    if err != nil {
+      log.Fatal(err)
+    }
+    
+    fmt.Printf("wei: %s\n", bal) // "wei: 74605500647408739782407023"
+    //fmt.Fprint(w, "Hello, World!gae")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/youkchan/icb_faucet
 
 go 1.13
+
+require github.com/ethereum/go-ethereum v1.9.19
