938f7584dca77700fcbf7a0fe84a8eb5e698de7f
Yuki Tanaka <j1307099@yahoo.co.jp>
2020-08-21 22:46:39 +0900

    ipaddr

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
     //"fmt"
-    "bytes"
+//    "bytes"
 //    "context"
     "log"
     "net/http"
-    "net"
+//    "net"
     "os"
-    "strings"
+//    "strings"
     "strconv"
     "html/template"
 //    "crypto/ecdsa"
@@ -23,6 +23,7 @@ import (
 //    "github.com/ethereum/go-ethereum/core/types"
 //    "golang.org/x/crypto/sha3"
     ethereum "github.com/youkchan/icb_faucet/pkg/ethereum"
+    ipaddr "github.com/youkchan/icb_faucet/pkg/ipaddr"
 //    "reflect"
 )
 
@@ -64,7 +65,6 @@ func returnErrorParams(message string) Params {
 
     return params
 }
-
 
 func sendHandler(w http.ResponseWriter, r *http.Request) {
     t := template.Must(template.ParseFiles("web/html/index.html"))
@@ -128,83 +128,6 @@ func sendHandler(w http.ResponseWriter, r *http.Request) {
 
     t.Execute(w, params)
 }
-
-func getIPAdress(r *http.Request) string {
-    for _, h := range []string{"X-Forwarded-For", "X-Real-Ip"} {
-        addresses := strings.Split(r.Header.Get(h), ",")
-        // march from right to left until we get a public address
-        // that will be the address right before our proxy.
-        for i := len(addresses) -1 ; i >= 0; i-- {
-            ip := strings.TrimSpace(addresses[i])
-            // header can contain spaces too, strip those out.
-            realIP := net.ParseIP(ip)
-            if !realIP.IsGlobalUnicast() || isPrivateSubnet(realIP) {
-                // bad address, go to next
-                continue
-            }
-            return ip
-        }
-    }
-    return ""
-}
-
-type ipRange struct {
-    start net.IP
-    end net.IP
-}
-
-// inRange - check to see if a given ip address is within a range given
-func inRange(r ipRange, ipAddress net.IP) bool {
-    // strcmp type byte comparison
-    if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
-        return true
-    }
-    return false
-}
-
-var privateRanges = []ipRange{
-    ipRange{
-        start: net.ParseIP("10.0.0.0"),
-        end:   net.ParseIP("10.255.255.255"),
-    },
-    ipRange{
-        start: net.ParseIP("100.64.0.0"),
-        end:   net.ParseIP("100.127.255.255"),
-    },
-    ipRange{
-        start: net.ParseIP("172.16.0.0"),
-        end:   net.ParseIP("172.31.255.255"),
-    },
-    ipRange{
-        start: net.ParseIP("192.0.0.0"),
-        end:   net.ParseIP("192.0.0.255"),
-    },
-    ipRange{
-        start: net.ParseIP("192.168.0.0"),
-        end:   net.ParseIP("192.168.255.255"),
-    },
-    ipRange{
-        start: net.ParseIP("198.18.0.0"),
-        end:   net.ParseIP("198.19.255.255"),
-    },
-}
-
-
-// isPrivateSubnet - check to see if this ip is in a private subnet
-func isPrivateSubnet(ipAddress net.IP) bool {
-    // my use case is only concerned with ipv4 atm
-    if ipCheck := ipAddress.To4(); ipCheck != nil {
-        // iterate over all our ranges
-        for _, r := range privateRanges {
-            // check if this ip is in a private range
-            if inRange(r, ipAddress){
-                return true
-            }
-        }
-    }
-    return false
-}
-
 func indexHandler(w http.ResponseWriter, r *http.Request) {
     Env_load()
     t := template.Must(template.ParseFiles("web/html/index.html"))
@@ -213,7 +136,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
             return
     }
 
-    ip := getIPAdress(r)
+    ip := ipaddr.GetIPAdress(r)
 
 
 /*    network_list := []ethereum.Network{
